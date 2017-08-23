736b5e5f5b9c336f3866e791fd2422427050d2cf
Shiela Mae Bobadilla <shielamaebobadilla@gmail.com>
2017-08-23 20:57:32 +0800

    Implement a page for showing nearby laundry shops.
    - Create ListController for showing a list of nearby laundry shops.
    - Display a loading screen while detecting location and retrieving nearby shops.

---
+++ src/CodingAvenue/LabadoBundle/Controller/ListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CodingAvenue\LabadoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ListController extends Controller
+{
+    /**
+     * @Route("laundryshops")
+     */
+    public function indexAction()
+    {
+       return $this->render('CodingAvenueLabadoBundle:List:index.html.twig');
+    }
+
+}
