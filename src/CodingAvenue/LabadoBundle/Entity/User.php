8b0cedb1956619787f678ab40cfe33c1c78410c8
R-jay Villegas <officialrjayv@gmail.com>
2017-09-06 17:59:32 +0800

    modify User entity to add landmark, contact number, and address

--- src/CodingAvenue/LabadoBundle/Entity/User.php
+++ src/CodingAvenue/LabadoBundle/Entity/User.php
@@ -18,9 +18,85 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255,nullable=true)
+     */
+    private $address;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="landmark", type="string", length=255,nullable=true)
+     */
+    private $landmark;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contact_number", type="string", length=255,nullable=true)
+     */
+    private $contactNumber;
+
+    
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+    
+    /**
+     * Set address 
+     *
+     * @param String $address
+     * @return User
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    
+        return $this;
+    }
+
+    public function getLandmark()
+    {
+        return $this->landmark;
+    }
+    
+    /**
+     * Set landmark 
+     *
+     * @param String $landmark
+     * @return User
+     */
+    public function setLandmark($landmark)
+    {
+        $this->landmark = $landmark;
+    
+        return $this;
+    }
+
+    public function getContactNumber()
+    {
+        return $this->contactNumber;
+    }
+    
+    /**
+     * Set contactNumber 
+     *
+     * @param String $contactNumber
+     * @return User
+     */
+    public function setContactNumber($contactNumber)
+    {
+        $this->contactNumber = $contactNumber;
+    
+        return $this;
     }
 }
