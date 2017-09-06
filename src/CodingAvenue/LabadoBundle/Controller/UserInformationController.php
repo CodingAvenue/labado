3e42e04dbee6c0fe85bedfddd0e9c1cbc4807529
R-jay Villegas <officialrjayv@gmail.com>
2017-09-06 22:58:33 +0800

    Add the user's update information for the first time setup and updates

---
+++ src/CodingAvenue/LabadoBundle/Controller/UserInformationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CodingAvenue\LabadoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use CodingAvenue\LabadoBundle\Entity\User;
+use CodingAvenue\LabadoBundle\Form\InformationType;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserInformationController extends Controller
+{
+    /**
+     * @Route("/profile/info/edit", name="user_info_edit")
+     */
+    public function editAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $user = $em->getRepository(User::class)->find($user);
+
+        $form = $this->createForm(InformationType::class,$user ); 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $user->setAddress($data->getAddress());
+            $user->setLandmark($data->getLandmark());
+            $user->setContactNumber($data->getContactNumber());
+            $em->flush();
+        }
+        return $this->render('CodingAvenueLabadoBundle:UserInformation:edit.html.twig', [ 
+           "form" => $form->createView(), 
+        ]);
+    }
+
+}
