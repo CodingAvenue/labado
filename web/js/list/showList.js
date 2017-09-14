d03fd9d65099f465b4cdd4b8281a2da5b28e90ea
Marl <mjnegre9@gmail.com>
2017-09-14 17:13:21 +0800

    Showlist for laundryshops is modified.
    - Add one detail to showlist which is the vicinity.

--- web/js/list/showList.js
+++ web/js/list/showList.js
@@ -53,7 +53,9 @@ function success(position){
 function renderList(laundry_shops){
     var ul_id = $('#laundry-list');            
         laundry_shops.map(function(laundry){                    
-        var row = "<li><div class=\"laundry-header collapsible-header\" data-target="+laundry.place_id+"><i class=\"material-icons\">local_laundry_service</i>"+laundry.name+"</div> <div class=\"collapsible-body\"></div></li>";
+        var row = '<li><div class="laundry-header collapsible-header" data-target='+laundry.place_id
+        +'><i class="material-icons">local_laundry_service</i><p><span>'+laundry.name+'</span><span id="vici">'
+        +laundry.vicinity+'</span></p></div><div class="collapsible-body"></div></li>';
         ul_id.append(row);
     });
 }
