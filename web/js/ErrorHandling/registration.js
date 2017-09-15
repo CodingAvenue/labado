07f500698ed8eb5311dede1e31acae6a32367efb
R-jay Villegas <officialrjayv@gmail.com>
2017-09-15 17:31:38 +0800

    Modify to finalize the registration validation

--- web/js/ErrorHandling/registration.js
+++ web/js/ErrorHandling/registration.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     var timeout = null
 
-    $("#registration").on('focusout keyup', '.validate', function(e) {
+    $("#registration").on('keyup focusout', '.validate', function(e) {
         // this will avoid the keyup key 9 which is the "Tab" key 
         // this is to avoid double request when the focusout event triggered cause of keyup event
         if(!(e.type == 'keyup' && e.which == 9)) {
             target = $(this);
-            var url = (target.attr('name') == "form_user_registration_form_email") ? "/registration/exist/email" : "/registration/exist/username";
+            var url = (target.attr('id') == "fos_user_registration_form_email") ? "/registration/exist/email" : "/registration/exist/username";
             clearTimeout(timeout);
             timeout = setTimeout(function () {
                 $.ajax({
@@ -17,10 +17,11 @@ $(document).ready(function() {
                     success: function(data) {
                         if(data['status']) {
                             target.addClass("invalid");
+                            target.removeClass("valid");
                             target.next('.formlabel').attr('data-error', target.next('.formlabel').text() + ' Already Taken');
                         } else {
                             target.addClass("valid");
-                            target.next('.formlabel').attr('data-error', 'Invalid ' + target.next('.formlabel').text());
+                            target.next('.formlabel').attr('data-error', (url == "/registration/exist/email" ) ? 'Invalid Email' : 'Username must be atleast 4 characters' );
                         }
                     },
                 });
@@ -29,17 +30,33 @@ $(document).ready(function() {
     });
 
     $("#registration").on('focusout keyup', '.password-input', function(e) {
-        target = $('#fos_user_registration_form_plainPassword_second'); 
-        target.next('.formlabel').attr('data-error', 'Password did not match ');
+        shortPasswordTarget= $('#fos_user_registration_form_plainPassword_first'); 
+        matchPasswordTarget = $('#fos_user_registration_form_plainPassword_second'); 
 
-        var first = $('#fos_user_registration_form_plainPassword_first').val();
-        var second = target.val();
+        var firstPassword = shortPasswordTarget.val(); 
+        var secondPassword = matchPasswordTarget.val();
 
-        if(first && second ) {
-            target.addClass( (first!=second)? "invalid": "valid" );
-        }else{
-            target.addClass("valid");
-        }
+       if($(this).attr('id') == shortPasswordTarget.attr('id') && firstPassword) {
+            if(firstPassword.length < 8) {
+                shortPasswordTarget.addClass('invalid');
+                shortPasswordTarget.next('.formlabel').attr('data-error', 'Password must be atleast 8 characters ');
+
+            } else {
+                shortPasswordTarget.removeClass('invalid');
+                shortPasswordTarget.addClass('valid');
+            }
+       } else {
+            if(firstPassword != secondPassword && firstPassword && secondPassword) {
+                matchPasswordTarget.addClass('invalid');
+                matchPasswordTarget.next('.formlabel').attr('data-error', 'Passwords did not match');
+
+            } else {
+                matchPasswordTarget.removeClass('invalid');
+                matchPasswordTarget.addClass('valid'); 
+
+            }
+       }
+
     });
 
 });
