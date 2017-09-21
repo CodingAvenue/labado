b2517feaabba1aba561f9e208ff439f66538fb11
shielaboba <shielamaebobadilla@gmail.com>
2017-09-21 17:46:46 +0800

    Clean code in css and js file

--- web/js/list/showList.js
+++ web/js/list/showList.js
@@ -52,18 +52,20 @@ $(function () {
     }
 
     function renderList(laundry_shops) {
+        var row="";
         var ul_id = $('#laundry-list');
         laundry_shops.map(function (laundry) {
-            var row = '<li><div class="laundry-header collapsible-header" data-target=' + laundry.place_id +
-                '><i class="material-icons">local_laundry_service</i>' + laundry.name +
-                '</div> <div class="collapsible-body"></div></li>';
-            ul_id.append(row);
+            row += '<li><div class="laundry-header collapsible-header" data-target=' + laundry.place_id
+                + '><i class="material-icons">local_laundry_service</i><p><span>'
+                + laundry.name + '</span><span id="vici">'
+                + laundry.vicinity + '</span></p></div><div class="collapsible-body"></div></li>';
         });
+            console.log("w");
+            ul_id.append(row);
     }
 
     function listResult(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
-            listShop = results;
             $('#load-page').hide();
             $('#list-container').show();
             renderList(results);
@@ -158,14 +160,14 @@ $(function () {
     $("#laundry-list").on('click', '.laundry-header', function () {
         var placeId = $(this).data('target');
         var theUrl = "laundryshop/"+ placeId + "/details";
-        var body = $(this).next('.collapsible-body');
+        var container_body = $(this).next('.collapsible-body');
         $.ajax({
             url: theUrl,
         }).done(function (data) {
-            body.html(data);
+            container_body.html(data);
         }).fail(function (e) {
             var errorMessage = "Cannot retrieve data";
-            body.html(errorMessage);
+            container_body.html(errorMessage);
         });
     });
 
