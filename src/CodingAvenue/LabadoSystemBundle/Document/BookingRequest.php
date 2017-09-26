dce91bd5ed2a6763e1c8487562567d1cfbf6f8b0
R-jay Villegas <officialrjayv@gmail.com>
2017-09-26 15:13:55 +0800

    Create the booking request entity

---
+++ src/CodingAvenue/LabadoSystemBundle/Document/BookingRequest.php
@@ -0,0 +1,168 @@
+<?php
+namespace CodingAvenue\LabadoSystemBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document
+ */
+ class BookingRequest
+ {
+    /** @MongoDB\Id */
+    public $id;
+
+    /** @MongoDB\Field(type="dateTime") */
+    public $requested_at;
+
+    /**
+    * @MongoDB\ReferenceOne(targetDocument="User")
+    */
+    protected $user;
+
+    /**
+    * @MongoDB\ReferenceOne(targetDocument="LaundryShop")
+    */
+    protected $laundryShop;
+    
+    /**
+    * @MongoDB\EmbedMany(targetDocument="StandardServiceMatrix")
+    */
+    protected $standardServiceMatrix;
+
+    /**
+    * @MongoDB\Field(type="string")
+    */
+    protected $status;
+     public function __construct()
+    {
+        $this->standardServiceMatrix = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set requestedAt
+     *
+     * @param dateTime $requestedAt
+     * @return self
+     */
+    public function setRequestedAt(\dateTime $requestedAt)
+    {
+        $this->requested_at = $requestedAt;
+        return $this;
+    }
+
+    /**
+     * Get requestedAt
+     *
+     * @return dateTime $requestedAt
+     */
+    public function getRequestedAt()
+    {
+        return $this->requested_at;
+    }
+
+    /**
+     * Set user
+     *
+     * @param CodingAvenue\LabadoSystemBundle\Document\User $user
+     * @return self
+     */
+    public function setUser(\CodingAvenue\LabadoSystemBundle\Document\User $user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return CodingAvenue\LabadoSystemBundle\Document\User $user
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set laundryShop
+     *
+     * @param CodingAvenue\LabadoSystemBundle\Document\LaundryShop $laundryShop
+     * @return self
+     */
+    public function setLaundryShop(\CodingAvenue\LabadoSystemBundle\Document\LaundryShop $laundryShop)
+    {
+        $this->laundryShop = $laundryShop;
+        return $this;
+    }
+
+    /**
+     * Get laundryShop
+     *
+     * @return CodingAvenue\LabadoSystemBundle\Document\LaundryShop $laundryShop
+     */
+    public function getLaundryShop()
+    {
+        return $this->laundryShop;
+    }
+
+    /**
+     * Add standardServiceMatrix
+     *
+     * @param CodingAvenue\LabadoSystemBundle\Document\StandardServiceMatrix $standardServiceMatrix
+     */
+    public function addStandardServiceMatrix(\CodingAvenue\LabadoSystemBundle\Document\StandardServiceMatrix $standardServiceMatrix)
+    {
+        $this->standardServiceMatrix[] = $standardServiceMatrix;
+    }
+
+    /**
+     * Remove standardServiceMatrix
+     *
+     * @param CodingAvenue\LabadoSystemBundle\Document\StandardServiceMatrix $standardServiceMatrix
+     */
+    public function removeStandardServiceMatrix(\CodingAvenue\LabadoSystemBundle\Document\StandardServiceMatrix $standardServiceMatrix)
+    {
+        $this->standardServiceMatrix->removeElement($standardServiceMatrix);
+    }
+
+    /**
+     * Get standardServiceMatrix
+     *
+     * @return \Doctrine\Common\Collections\Collection $standardServiceMatrix
+     */
+    public function getStandardServiceMatrix()
+    {
+        return $this->standardServiceMatrix;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     * @return self
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string $status
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
