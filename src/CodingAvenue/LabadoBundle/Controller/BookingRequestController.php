0f9c81b606f69b06522916f0aba17680d0fcbf97
R-jay Villegas <officialrjayv@gmail.com>
2017-09-26 18:23:47 +0800

    Initial Commit for BookingRequestController

--- src/CodingAvenue/LabadoBundle/Controller/BookingRequestController.php
+++ src/CodingAvenue/LabadoBundle/Controller/BookingRequestController.php
@@ -4,6 +4,8 @@ namespace CodingAvenue\LabadoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use CodingAvenue\LabadoSystemBundle\Document\BookingRequest;
+use Eo\JobQueueBundle\Document\Job;
 
 class BookingRequestController extends Controller
 {
@@ -11,7 +13,17 @@ class BookingRequestController extends Controller
      * @Route("/bookingrequest", name="bookingRequest")
      */
     public function storeAction()
-    {
+        {
+        $job = new Job('book', []);
+
+        $br = new BookingRequest();
+
+        $br->setUser($this->container->get('security.token_storage')->getToken()->getUser());
+
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
+
         return $this->render('CodingAvenueLabadoBundle:BookingRequest:store.html.twig', array(
             // ...
         ));
