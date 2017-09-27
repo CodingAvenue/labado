a50ba215ee2163c2345965995091f6acc912709b
shielaboba <shielamaebobadilla@gmail.com>
2017-09-27 16:21:43 +0800

    Store user's coordinates to User entity via POST.

--- src/CodingAvenue/LabadoBundle/Controller/LocationController.php
+++ src/CodingAvenue/LabadoBundle/Controller/LocationController.php
@@ -4,6 +4,11 @@ namespace CodingAvenue\LabadoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use CodingAvenue\LabadoSystemBundle\Document\Coordinates;
+use CodingAvenue\LabadoSystemBundle\Document\User;
 
 class LocationController extends Controller
 {
@@ -17,4 +22,24 @@ class LocationController extends Controller
         ));
     }
 
+    /**
+     * @Route("/location", name="store")
+     * @Method("POST")
+     */
+    public function storeAction(Request $request)
+    {
+        $user = $this->container->get("security.token_storage")->getToken()->getUser();
+
+        $coords = new Coordinates();
+        $coords->setX($request->get("x"));
+        $coords->setY($request->get("y"));
+
+        $user->setCoordinates($coords);
+
+        $mongo_em = $this->get('doctrine_mongodb')->getManager();
+        $mongo_em->flush();
+
+        $response = new JsonResponse($user->getCoordinates());
+        return $response;
+    }
 }
--- src/CodingAvenue/LabadoSystemBundle/Document/User.php
+++ src/CodingAvenue/LabadoSystemBundle/Document/User.php
@@ -13,4 +13,31 @@ class User extends BaseUser
      * @MongoDB\Id(strategy="auto")
      */
     protected $id;
+
+    /** @MongoDB\EmbedOne(targetDocument="Coordinates") */
+    public $coordinates;
+
+    // @todo add Coordinates embeded entity here. See Labadoer $coordinates property
+
+    /**
+     * Set coordinates
+     *
+     * @param CodingAvenue\LabadoSystemBundle\Document\Coordinates $coordinates
+     * @return self
+     */
+    public function setCoordinates(\CodingAvenue\LabadoSystemBundle\Document\Coordinates $coordinates)
+    {
+        $this->coordinates = $coordinates;
+        return $this;
+    }
+
+    /**
+     * Get coordinates
+     *
+     * @return CodingAvenue\LabadoSystemBundle\Document\Coordinates $coordinates
+     */
+    public function getCoordinates()
+    {
+        return $this->coordinates;
+    }
 }
