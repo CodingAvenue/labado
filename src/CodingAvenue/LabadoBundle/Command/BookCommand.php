d0445049d966707a123626f9ce20f9de9694b609
Marl Negre <mjnegre9@gmail.com>
2017-09-28 16:48:53 +0800

    Book Command is implemented.

--- src/CodingAvenue/LabadoBundle/Command/BookCommand.php
+++ src/CodingAvenue/LabadoBundle/Command/BookCommand.php
@@ -7,6 +7,12 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use CodingAvenue\LabadoSystemBundle\Repository\LabadoerRepository;
+use CodingAvenue\LabadoSystemBundle\Document\BookingRequest;
+use CodingAvenue\LabadoSystemBundle\Document\Booking;
+use CodingAvenue\LabadoSystemBundle\Document\Labadoer;
+use CodingAvenue\LabadoSystemBundle\Document\User;
+use DateTime;
 
 class BookCommand extends ContainerAwareCommand
 {
@@ -14,7 +20,7 @@ class BookCommand extends ContainerAwareCommand
     {
         $this
             ->setName('book')
-            ->setDescription('...')
+            ->setDescription('This command will do the actual booking of the user.')
             ->addArgument('booking_request_id', InputArgument::OPTIONAL, 'BookingRequest ID');
         ;
     }
@@ -22,7 +28,37 @@ class BookCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $booking_request_id = $input->getArgument('booking_request_id');
+        
+        $em = $this->getContainer()->get('doctrine_mongodb')->getManager();
+        $booking_request = $em->getRepository(BookingRequest::class)->find($booking_request_id);
 
+        $booking_request->setStatus(BookingRequest::STATUS_IN_PROGRESS);
+
+        $repository = $em->getRepository(Labadoer::class);
+        $coords = $booking_request->getUser()->getCoordinates();
+        $labadoer = $repository->findOneNearby($coords);
+
+
+        if($labadoer == null) {
+            $booking_request->setStatus(BookingRequest::STATUS_NO_LABADOER);
+        } else {
+            $booking = new Booking(); 
+            $booking->setBookedAt(new DateTime());
+            $booking->setLabadoer($labadoer);
+            $booking->setUser($booking_request->getUser());
+
+            $services = $booking_request->getStandardServiceMatrix();
+            foreach($services as $service) {
+                $booking_request->addStandardServiceMatrix($service);
+            }
+
+            $booking->setLaundryShop($booking_request->getLaundryShop());
+            $booking->setStatus(Booking::STATUS_IN_PROGRESS);
+
+            $booking_request->setStatus(BookingRequest::STATUS_CONFIRMED);
+        }
+        
+        $em->flush();
         $output->writeln('Command result.');
     }
 
