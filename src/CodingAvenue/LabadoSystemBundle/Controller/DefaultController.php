cbcc22b40fcfb539aadde61d12d9d65db6dc43ab
jaydarken <officialrjayv@gmail.com>
2017-12-06 14:08:25 +0800

    Add defaultControoler to LabadoSystemBundle The first one was deleted

---
+++ src/CodingAvenue/LabadoSystemBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodingAvenue\LabadoSystemBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use CodingAvenue\LabadoSystemBundle\Document\User;
+use CodingAvenue\LabadoSystemBundle\Document\Labadoer;
+use CodingAvenue\LabadoSystemBundle\Document\Coordinates;
+
+class DefaultController extends Controller
+{
+    
+}
